77a180a537d721a785a45a1199173ad75d2adb3a
Aaron West <trajik210@gmail.com>
2016-06-28 15:03:05 -0500

    feat(fibonacci): add scaffolding to second example

--- fibonacci/README.md
+++ fibonacci/README.md
@@ -3,3 +3,4 @@
 These are different methods for producing the [Fibonacci sequence](https://en.wikipedia.org/wiki/Fibonacci_number) in Python.
 
 * **fibonacci.py** - use an array and while loop to generate _N_ number of Fibonacci numbers
+* **fibonacci2.py** - use generators and the yield keyword to print numbers of the Fibonacci sequence between _start number_ and _end number_
--- fibonacci/fibonacci2.py
+++ fibonacci/fibonacci2.py
@@ -1,7 +1,46 @@
+import sys
+import getopt
+
 __author__ = "Aaron S. West"
 
+def usage():
+    print sys.argv[0]
+    usage = """
+    Required parameters:
+    -s              : the start number
+    -e              : the end number
+    """
+    print 'usage: ' + sys.argv[0] + ' -s <integer> -e <integer>'
+    print usage
+
+def main(argv):
+    startNumber = 0
+    endNumber = 0
+
+   # Test input args.
+    try:
+      opts, args = getopt.getopt(argv,"hs:e:",["help"])
+    except getopt.GetoptError:
+       usage()
+       sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt == '-s':
+            startNumber = int(arg)
+        elif opt == '-e':
+            endNumber = int(arg)
+
+    # Ensure iteration count. Exit with an appropriate message if not.
+    if startNumber == 0 or endNumber == 0:
+        sys.exit("Start and end number (-s, -e) are required. See 'python " + sys.argv[0] + " -h'")
+
+    for i in fib_generator(startNumber, endNumber):
+        print i
+
 def F():
-    #print "F start"
+    # print "F start"
     a,b = 0,1
     yield a
     yield b
@@ -9,12 +48,12 @@ def F():
         a, b = b, a + b
         yield b
 
-def SubFib(startNumber, endNumber):
+def fib_generator(startNumber, endNumber):
     for cur in F():
-        #print "SubFib " + str(cur)
+        # print "SubFib " + str(cur)
         if cur > endNumber: return
         if cur >= startNumber:
             yield cur
 
-for i in SubFib(0, 50):
-    print i
+if __name__ == "__main__":
+    main(sys.argv[1:])
